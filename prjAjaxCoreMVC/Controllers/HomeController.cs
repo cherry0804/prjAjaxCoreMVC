de8757c31153a85baf0521c05bcee541d12591f9
Jacky Jie <jakcy2222@gmail.com>
2023-11-14 16:10:33 +0800

    Ajax第一個程式

--- prjAjaxCoreMVC/Controllers/HomeController.cs
+++ prjAjaxCoreMVC/Controllers/HomeController.cs
@@ -28,5 +28,10 @@ namespace prjAjaxCoreMVC.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
+        public IActionResult First()
+        {
+            return View();
+        }
     }
 }
