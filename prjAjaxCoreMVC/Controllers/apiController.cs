b4effcbd8ab33637c23ad734b3b766010dbcad72
Jacky Jie <jakcy2222@gmail.com>
2023-11-14 15:31:24 +0800

    新增一個api Controller 寫一個Hello ajax

---
+++ prjAjaxCoreMVC/Controllers/apiController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace prjAjaxCoreMVC.Controllers
+{
+    public class apiController : Controller
+    {
+        public IActionResult Index()
+        {
+            return Content("Hello Ajax!!");
+        }
+    }
+}
